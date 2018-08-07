39f57f77fe2e2a93656ece4ac898a161c8259daa
Morten Olsen <morten@olsen.pro>
2018-08-07 12:48:29 +0200

    Updated password checker

--- init.sh
+++ init.sh
@@ -29,3 +29,7 @@ do
   unset PACKAGE_DIR
 done
 unset PACKAGES
+
+export NVM_DIR="$HOME/Projects/sampension/frontend/app/.build-tools/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
